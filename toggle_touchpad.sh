660e483f562eb58d08622680adde3ebc9cb0c6be
Maurício Gruppi <mauricio.gruppi@gmail.com>
2019-03-21 17:16:38 -0400

    Fixed messages

--- toggle_touchpad.sh
+++ toggle_touchpad.sh
@@ -1,10 +1,10 @@
 # toggles touchpad on gnome3. Useful if you don't have a toggle touchpad key.
 t=$(gsettings get org.gnome.desktop.peripherals.touchpad send-events)
-echo "touchpad $t"
 if [ "$t" = "'enabled'" ]; then
 		gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled
+		echo " - Touchpad is now DISABLED."
 	else
 		gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
+		echo " - Touchpad is now ENABLED."
 fi
-
 
